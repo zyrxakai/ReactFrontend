8fe6d7e97f9630b6ed5ca586fb40812aa35f85e5
zyrxakai <akshaib7557@gmail.com>
2024-07-28 11:08:47 +0530

    all are done ,need to change or add website name and properties

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,17 +7,20 @@ const Navbar = () => {
       <nav className="flex w-full screen-max-width">
        
 
-        <div className="flex flex-1 justify-center max-sm:hidden">
+        {/* <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
             <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
               {nav}
             </div>
           ))}
-        </div>
+        </div> */}
 
-        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-          <img className='cursor:pointer' src={searchImg} alt="search" width={18} height={18} />
-          <img src={bagImg} alt="bag" width={18} height={18} />
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1 ml-[950px] mt-2 ">
+        
+        <li className='text-gray hover:text-white'><a href="#">Shop</a></li>
+        <li className='text-gray hover:text-white'><a href="Services">Services</a></li>
+        
+
         </div>
       </nav>
     </header>
